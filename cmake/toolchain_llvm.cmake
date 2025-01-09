aac854d62e54a97a4ceb784ddb4e7a470972d98d
Philip Wiese <wiesep@iis.ee.ethz.ch>
2025-01-09 15:54:15 +0100

    Fix Linker Issues and Update Build Documentation (#20)
    * Fix wrong usage instructions
    
    * Switch to LLD and disable linker relaxation for LLVM<15
    
    * Update README
    
    * Minor cleanup
    
    * Implement PR feedback

--- cmake/toolchain_llvm.cmake
+++ cmake/toolchain_llvm.cmake
@@ -11,13 +11,37 @@ set(CMAKE_SYSTEM_NAME Generic)
 
 set(LLVM_TAG llvm)
 
-set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/clang)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/clang++)
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN_DIR}/clang)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/clang)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/clang++)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_DIR}/bin/clang)
 
-set(CMAKE_OBJCOPY ${TOOLCHAIN_DIR}/${LLVM_TAG}-objcopy)
-set(CMAKE_OBJDUMP ${TOOLCHAIN_DIR}/${LLVM_TAG}-objdump)
-set(CMAKE_AR ${TOOLCHAIN_DIR}/${LLVM_TAG}-ar)
+set(CMAKE_OBJCOPY ${TOOLCHAIN_DIR}/bin/${LLVM_TAG}-objcopy)
+set(CMAKE_OBJDUMP ${TOOLCHAIN_DIR}/bin/${LLVM_TAG}-objdump)
+set(CMAKE_AR ${TOOLCHAIN_DIR}/bin/${LLVM_TAG}-ar)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=riscv32-unknown-elf")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --target=riscv32-unknown-elf")
+# Disable ABI detection
+set(CMAKE_C_ABI_COMPILED "False")
+
+add_compile_options("--target=riscv32-unknown-elf")
+
+# Use LLVM LLD linker
+add_link_options("-fuse-ld=lld")
+
+# Check LLVM version
+execute_process(
+    COMMAND ${CMAKE_C_COMPILER} --version
+    OUTPUT_VARIABLE LLVM_VERSION
+)
+string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" LLVM_VERSION ${LLVM_VERSION})
+
+# Get Major version
+string(REGEX MATCH "^[0-9]+" LLVM_VERSION_MAJOR ${LLVM_VERSION})
+string(REGEX MATCH "[0-9]+$" LLVM_VERSION_MINOR ${LLVM_VERSION})
+string(REGEX MATCH "[0-9]+$" LLVM_VERSION_PATCH ${LLVM_VERSION})
+
+if (LLVM_VERSION_MAJOR LESS 15)
+    message(STATUS "Disable linker relaxation for LLVM < 15")
+    # WIESEP: Disable linker relaxation for LLVM 12
+    add_compile_options("-mno-relax")
+    add_link_options("-Wl,--no-relax")
+endif()
